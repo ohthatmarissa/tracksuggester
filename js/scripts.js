5b88f990888d3b55b46b51a62fbef12f78deeddb
Marissa Perry <ohthatmarissa@gmail.com>
2019-03-29 15:35:20 -0700

    has the remaining if/else equasions and debugged

--- js/scripts.js
+++ js/scripts.js
@@ -14,11 +14,29 @@ $("form#questions".submit(function(event) {
       course = "Java";
     }
   }
+
+  if (customers === "no" && company === "company") {
+    if (design === "no") {
+    course = "C#";
+  } else if (design === "yes") {
+    course = "CSS/Design";
+  }
+}
+
+  if (design === "yes" && environment === "remotely") {
+    if (projects === "small") {
+    course = "PHP";
+  } else if (projects === "elaborate") {
+    course = "Ruby/Rails";
+  }
+}
+
 alert (check)
   $("#course").text("Your best course match is " + course + "!");
     $("#course").show();
 
     event.preventDefault();
+
 
   });
 });
